595534aa44decfe7ec1b539bb824368f34bdff19
Khanjan Bhatt <bhattkhanjan17@gmail.com>
2026-01-30 12:57:03 +0530

    vite.config.ts and app.tsx updated

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/Inhance-consulting/" : "/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
